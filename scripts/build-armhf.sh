8fbacf9f26d8910c9b36fce4029b2ad447a4decd
Philipp Dörfler <ph@doerfler.io>
2021-10-15 09:38:44 +0200

    Update build-armhf.sh

--- scripts/build-armhf.sh
+++ scripts/build-armhf.sh
@@ -7,8 +7,7 @@ NAME=`basename ${DIR}`
 SHA=`git rev-parse --short HEAD`
 VERSION=${VERSION:-$SHA}
 
-mkdir -p dist/armhf
-GOOS=linux GOARCH=arm go build . -o dist/armhf
+GOOS=linux GOARCH=arm go build .
 
 docker build -t nshttpd/${NAME}:${VERSION}-armhf -f Dockerfile.armhf .
 docker push nshttpd/${NAME}:${VERSION}-armhf
